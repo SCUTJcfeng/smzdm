8368663ec9cdf7a19a1c7dd4ad1db5b34e70b4c8
fengjianchao <jcfeng2013@gmail.com>
2019-08-18 13:05:26 +0800

    update add time validation

--- analysis.py
+++ analysis.py
@@ -1,5 +1,8 @@
 
 
+import datetime
+
+
 def keyword_analysis(data, maps):
     for sku_data in data['data']['rows']:
         article_url = sku_data['article_url']
@@ -8,23 +11,31 @@ def keyword_analysis(data, maps):
         article_price = sku_data['article_price']
         publish_date_lt = sku_data['publish_date_lt']
         print(article_title, article_url, article_mall, article_price, publish_date_lt)
+        if not is_time_valid(publish_date_lt):
+            continue
         if not is_match(article_title.upper(), maps):
             continue
         notify(format_msg(article_title, article_url, article_mall, article_price))
 
 
+def is_time_valid(time_int):
+    now = int(datetime.datetime.now().timestamp())
+    if (now - int(time_int)) > 60 * 5:
+        return False
+    return True
+
+
 def is_match(title, maps):
+    def is_keyword_match(title, keyword_list):
+        for keyword in keyword_list:
+            if keyword not in title:
+                return False
+        return True
+
     for keyword_list in maps:
         if is_keyword_match(title, keyword_list):
             return True
     return False
-
-
-def is_keyword_match(title, keyword_list):
-    for keyword in keyword_list:
-        if keyword not in title:
-            return False
-    return True
 
 
 def format_msg(title, url, mall, price):
